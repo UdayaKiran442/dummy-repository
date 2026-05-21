a34d3ba0ee43675d54d24e958b6248d33ba6a8df
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-05-21 13:43:48 +0530

    gg

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { add } from './utils'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/add', (c) => {
+  const result = add(5, 10)
+  return c.text(`Hello Hono! Result: ${result}`)
 })
 
 export default app
