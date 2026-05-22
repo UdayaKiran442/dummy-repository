b2b981c3e5651c15bab4a063c4b850495e9b83cd
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-05-22 16:37:45 +0530

    gg

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Hono } from 'hono'
 import { add } from './utls/utils'
+import { sub } from '../sub/sub'
 
 const app = new Hono()
 
 app.get('/add', (c) => {
   const result = add(5, 10)
+  return c.json({ result })
+})
+
+app.get('/sub', async (c) => {
+  const result = sub(10, 5)
   return c.json({ result })
 })
 
